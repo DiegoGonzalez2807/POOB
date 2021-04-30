291685fbe025c2aa4da07b04e364183c7aeb1891
cristian2306 <69209732+cristian2306@users.noreply.github.com>
2021-04-30 09:19:42 -0500

    Add files via upload

---
+++ LABORATORIOS/LAB6/domain/AutomataExcepcion.java
@@ -0,0 +1,19 @@
+package src.domain;
+/** 
+ * Contiene la informacion de los posibles errores que se den en el codigo
+ * @author Cristian Castellanos- Diego Gonzalez
+ * @version 2.0
+*/
+
+public class AutomataExcepcion extends Exception{
+    public static final String OPCION_CONSTRUCCION = "Opcion en construccion";
+
+    /**
+     * Crea una excepcion para este caso
+     * @param message Mensaje a mostrar
+     */
+
+    public AutomataExcepcion(String message){
+        super(message);
+    }
+}
