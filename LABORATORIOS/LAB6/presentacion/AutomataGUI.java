a087408c5ddd17d07026a9181096dd88adfdc36c
Diego Alejandro González <59978048+DiegoGonzalez2807@users.noreply.github.com>
2021-05-05 18:30:52 -0500

    Update AutomataGUI.java

--- LABORATORIOS/LAB6/presentacion/AutomataGUI.java
+++ LABORATORIOS/LAB6/presentacion/AutomataGUI.java
@@ -1,274 +1,168 @@
-package presentacion;
-import domain.*;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+package domain;
+import java.util.*;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.io.*;
-import javax.swing.JOptionPane;
-import javax.swing.filechooser.*;
 
-public class AutomataGUI extends JFrame{  
-    public static final int CELDA=21;
-    public static final int DIMENSION=CELDA*31;
-    
-    private JButton botonTicTac;
-    private JLabel lFila;
-    private JLabel lColumna;
-    private JTextField tFila;
-    private JTextField tColumna;
-    private JPanel panelControl;
-    private JPanel panelNueva;
-    private JPanel panelBNueva;
-    private JButton botonViva;
-    private JButton botonLatente;
-    private FotoAutomata foto;
-    private JMenuBar menu_bar = new JMenuBar();
-    private JMenu menu1;
-    private JMenuItem opcion1,opcion2,opcion3,opcion4,opcion5,opcion6;
-    
-    private AutomataCelular automata;
 
-    public AutomataGUI() {
-        automata=new AutomataCelular();
-        prepareElementos();
-        prepareAcciones();
+/*No olviden adicionar la documentacion*/
+public class AutomataCelular{
+    static private int LONGITUD=30;
+    private Elemento[][] automata;
+    private String sensor = "yellow";
+    
+    public AutomataCelular() {
+        automata=new Elemento[LONGITUD][LONGITUD];
+        for (int f=0;f<LONGITUD;f++){
+            for (int c=0;c<LONGITUD;c++){
+                automata[f][c]=null;
+            }
+        }
+        algunosElementos();
+    }
+
+    public int  getLongitud(){
+        return LONGITUD;
+    }
+
+    public Elemento getElemento(int f,int c){
+        return automata[f][c];
     }
     
-    private void prepareElementos() {
-        setTitle("Automata celular");
-        foto=new FotoAutomata(this);
-        botonTicTac=new JButton("Tic-tac");
-        setLayout(new BorderLayout());
-        add(foto,BorderLayout.NORTH);
-        add(botonTicTac,BorderLayout.SOUTH);
-        setSize(new Dimension(DIMENSION,DIMENSION+50)); 
-        setResizable(false);
-        foto.repaint();
-
-        //PREPARACION DEL MENU DE ARRIBA
-        prepareElementosMenu();
+    public String getSensor(){
+        return this.sensor;
     }
 
-    private void prepareElementosMenu(){
-        //CREADOR DEL MENU
-        menu();
-        //INSERTAR OPCIONES DEL MENU
-        opcionesMenu();
+    public void setElemento(int f, int c, Elemento nueva){
+        automata[f][c]=nueva;
     }
 
-    private void menu(){
-        setJMenuBar(menu_bar);
-    }
-
-    private void opcionesMenu(){
-        menu1 = new JMenu("Menu");
-        menu_bar.add(menu1);
-        opcion1 = new JMenuItem("Nuevo");
-        menu1.add(opcion1);
-        opcion2 = new JMenuItem("Abrir");
-        menu1.add(opcion2);
-        opcion3 = new JMenuItem("Guardar Como");
-        menu1.add(opcion3);
-        opcion4 = new JMenuItem("Importar");
-        menu1.add(opcion4);
-        opcion5 = new JMenuItem("Exportar Como");
-        menu1.add(opcion5);
-        opcion6 = new JMenuItem("Salir");
-        menu1.add(opcion6);
-    }
-
-    private void prepareAcciones(){
-        setDefaultCloseOperation(EXIT_ON_CLOSE);       
-        botonTicTac.addActionListener(
-            new ActionListener(){
-                public void actionPerformed(ActionEvent e) {
-                    botonTicTacAccion();
-                }
-            });
-        
-        //PREPARACION DE LOS BOTONES DEL MENU
-        prepareAccionesMenu();
-    }
-
-    private void botonTicTacAccion() {
-        automata.ticTac();
-        foto.repaint();
+    public static AutomataCelular nuevo(File file) throws AutomataExcepcion{
+        throw new AutomataExcepcion(AutomataExcepcion.OPCION_CONSTRUCCION);
     }
 
     /**
-     * Se ecarga de programar las funcionalidades de los botones del menu de opciones.
-     * Tener en cuenta de que cada a boton, cuando se quiera ejecutar va a saltar un error
-     * diciendo que los metodos siguen en desarrollo
-     * 
+     * Toma del archivo dado su informacion (automata,sensor y longitud)
+     * @param file Archivo en donde se encuentra la informacion
+     * @return Retorna el automata con la informacion del archivo
      */
-    private void prepareAccionesMenu(){
-        /* PREPARA UN NUEVO ARCHIVO */
-        opcion1.addActionListener(
-            new ActionListener(){
-                public void actionPerformed(ActionEvent e) {
-                    opcionNuevo();
-                }
-            });
-
-        /* ABRE UN NUEVO ARCHIVO */
-        opcion2.addActionListener(
-            new ActionListener(){
-                public void actionPerformed(ActionEvent e) {
-                    opcionAbrir();
-                }
-            });
-            
-        /* GUARDA UN ARCHIVO */
-        opcion3.addActionListener(
-            new ActionListener(){
-                public void actionPerformed(ActionEvent e) {
-                    opcionGuardar();
-                }
-            });
-
-        /* IMPORTA UN ARCHIVO */
-        opcion4.addActionListener(
-            new ActionListener(){
-                public void actionPerformed(ActionEvent e) {
-                    opcionImportar();
-                }
-            });
-
-        /* EXPORTA UN ARCHIVO */
-        opcion5.addActionListener(
-            new ActionListener(){
-                public void actionPerformed(ActionEvent e) {
-                    opcionExportar();
-                }
-            });
-
-        /* SALE DEL SIMULADOR */
-        opcion6.addActionListener(
-            new ActionListener(){
-                public void actionPerformed(ActionEvent e) {
-                    opcionSalir();
-                }
-            });
-            
+    public static AutomataCelular abrir(File file) throws AutomataExcepcion{
+        throw new AutomataExcepcion(AutomataExcepcion.OPCION_CONSTRUCCION);
+       
     }
 
-    private void opcionNuevo(){
-            AutomataCelular nuevo = new AutomataCelular();
-            this.setVisible(false);
-            this.automata = nuevo;
-            this.setVisible(true);
-
+    /**
+     * Toma la informacion del automata,elementos, longitud y demas, y la guarda.
+     * @param file Archivo en donde se guarda la informacion
+     */
+    public static void guardar(File file) throws AutomataExcepcion{
+        throw new AutomataExcepcion(AutomataExcepcion.OPCION_CONSTRUCCION);
     }
 
-    private void opcionAbrir(){
-        File archivo = new File("");
+    /**
+     * Toma del archivo dado su informacion (automata,sensor y longitud)
+     * @param file Archivo en donde se encuentra la informacion
+     * @return Retorna el automata con la informacion del archivo
+     */
+    public static AutomataCelular abra00(File file) throws AutomataExcepcion{
+        throw new AutomataExcepcion(AutomataExcepcion.OPCION_CONSTRUCCION);
+       
+    }
+
+    /**
+     * Toma la informacion del automata,elementos, longitud y demas, y la guarda.
+     * @param file Archivo en donde se guarda la informacion
+     */
+    public static void guarde00(AutomataCelular automata, File file) throws AutomataExcepcion{
+        //throw new AutomataExcepcion(AutomataExcepcion.OPCION_CONSTRUCCION);
+        if(!file.getName().endsWith(".dat")){
+            throw new AutomataExcepcion(AutomataExcepcion.EXTENSION_NO_VALIDA);
+        }
         try{
-            JFileChooser fileChooser = new JFileChooser();
-            //FILTRA TODOS LOS ARCHIVOS Y SOLO DEJA LOS VISIBLES
-            //LOS QUE TENGAN EXTENSION .DAT.
-            fileChooser.setFileFilter(new FileNameExtensionFilter("Archivo con extensión .DAT","DAT"));
-            int seleccion = fileChooser.showOpenDialog(this);
-            if (seleccion == JFileChooser.APPROVE_OPTION) {
-                archivo = fileChooser.getSelectedFile();
-            }
-            
-            automata.abrir(archivo);
-        }catch(AutomataExcepcion e){
-            JOptionPane.showMessageDialog(null, "Metodo en construccion, aun no se puede abrir archivos externos");
+            ObjectOutputStream archivoGuardado = new ObjectOutputStream(new FileOutputStream(file));
+            archivoGuardado.writeObject(automata);
+            archivoGuardado.close();
+        }
+        catch(IOException e){
+            throw new AutomataExcepcion("Ocurrio un error al tratar de salvar el archivo bajo el nombre" + file.getName());
         }
     }
 
-    private void opcionGuardar(){
-        File archivo = new File("");
-        try{
-            JFileChooser fileChooser = new JFileChooser();
-            //FILTRA TODOS LOS ARCHIVOS Y SOLO DEJA LOS VISIBLES
-            //LOS QUE TENGAN EXTENSION .DAT.
-            fileChooser.setFileFilter(new FileNameExtensionFilter("Archivo con extensión .DAT","DAT"));
-            int seleccion = fileChooser.showSaveDialog(this);
-            if (seleccion == JFileChooser.APPROVE_OPTION) {
-                archivo = fileChooser.getSelectedFile();
-            }
-            
-            automata.guardar(archivo);
-        }catch(AutomataExcepcion e){
-            JOptionPane.showMessageDialog(null, "Metodo en construccion, aun no se pueden guardar archivos");
-        }
+
+    /**
+     * Toma del archivo dado la informacion dada linea por linea
+     * @param file Archivo en donde se encuentra la informacion
+     * @return Retorna el automata con la informacion del archivo
+     */
+    public static AutomataCelular importar(File file) throws AutomataExcepcion{
+        throw new AutomataExcepcion(AutomataExcepcion.OPCION_CONSTRUCCION);
+        
     }
 
-    private void opcionImportar(){
-        try{
-            File archivo = new File("");
-            automata.importar(archivo);
-        }catch(AutomataExcepcion e){
-            JOptionPane.showMessageDialog(null, "Metodo en construccion, aun no se puede importar archivos");
-        }
+     /**
+     * Toma la informacion del automata y la guarda  en forma de texto, el formato designado es componente posicion
+     * @param file Archivo en donde se guarda la informacion
+     */
+    public static void exportar(File file) throws AutomataExcepcion{
+        throw new AutomataExcepcion(AutomataExcepcion.OPCION_CONSTRUCCION);
     }
 
-    private void opcionExportar(){
-        try{
-            File archivo = new File("");
-            automata.exportar(archivo);
-        }catch(AutomataExcepcion e){
-            JOptionPane.showMessageDialog(null, "Metodo en construccion, aun no se puede exportar archivos");
-        }
-    }
-
-    private void opcionSalir(){
-        System.exit(0);
-    }
-
-    public AutomataCelular getAutomata(){
-        return automata;
+    public void algunosElementos(){
+        SensorVida sensor_1 = new SensorVida(this,15,29);
+        SensorVida sensor_2 = new SensorVida(this,16,29);
+        SensorVida sensor_3 = new SensorVida(this,15,28);
+        SensorVida sensor_4 = new SensorVida(this,16,28);
+        Celula muerta = new Celula(this, 5,6);
+        setElemento(5,4,muerta);
+        setElemento(4,5,muerta);
+        ticTac();
+        ticTac();
+        ticTac();
+        Celula indiana = new Celula(this, 1,1);
+        Celula celula_007 = new Celula(this,2,2);
+        Celula agamenon = new CelulaEspecial(this,5,5);
+        Celula venus = new CelulaEspecial(this,10,10);
+        Calefactor suroeste = new Calefactor(this,0,29);
+        Calefactor noreste = new Calefactor(this,29,29);
+        Celula diego = new CelulaGenesis(this,20,20);
+        Celula cristian = new CelulaGenesis(this,6,23);
+        setElemento(0,29,suroeste);
+        setElemento(29,29,noreste);
+        CelulaConway con1 = new CelulaConway(this,14,15);
+        CelulaConway con2 = new CelulaConway(this,14,16);
+        prueba1();
+        prueba2();
+        prueba3();
     }
     
-    public static void main(String[] args) {
-        AutomataGUI ca=new AutomataGUI();
-        ca.setVisible(true);
-    }  
-}
-
-class FotoAutomata extends JPanel{
-    private AutomataGUI gui;
-
-    public FotoAutomata(AutomataGUI gui) {
-        this.gui=gui;
-        setBackground(Color.white);
-        setPreferredSize(new Dimension(gui.DIMENSION, gui.DIMENSION));         
+    private void prueba1(){
+        CelulaConway john = new CelulaConway(this,5,27);
+        CelulaConway horton = new CelulaConway(this,5,28);
     }
-
-
-    public void paintComponent(Graphics g){
-        AutomataCelular automata=gui.getAutomata();
-        super.paintComponent(g);
-         
-        for (int f=0;f<=automata.getLongitud();f++){
-            g.drawLine(f*gui.CELDA,0,f*gui.CELDA,automata.getLongitud()*gui.CELDA);
-        }
-        for (int c=0;c<=automata.getLongitud();c++){
-            g.drawLine(0,c*gui.CELDA,automata.getLongitud()*gui.CELDA,c*gui.CELDA);
-        }       
-        for (int f=0;f<automata.getLongitud();f++){
-            for(int c=0;c<automata.getLongitud();c++){
-                if (automata.getElemento(f,c)!=null){
-                    g.setColor(automata.getElemento(f,c).getColor());
-                    if (automata.getElemento(f,c).forma()==Elemento.CUADRADA){                  
-                        if (automata.getElemento(f,c).isVivo()){
-                            g.fillRoundRect(gui.CELDA*c+1,gui.CELDA*f+1,gui.CELDA-2,gui.CELDA-2,2,2);
-                        }else{
-                            g.drawRoundRect(gui.CELDA*c+1,gui.CELDA*f+1,gui.CELDA-2,gui.CELDA-2,2,2);    
-
-                        }
-                    }else {
-                        if (automata.getElemento(f,c).isVivo()){
-                            g.fillOval(gui.CELDA*c+1,gui.CELDA*f+1,gui.CELDA-2,gui.CELDA-2);
-                        } else {
-                            g.drawOval(gui.CELDA*c+1,gui.CELDA*f+1,gui.CELDA-2,gui.CELDA-2);
-                        }
-                    }
+    
+    private void prueba2(){
+        CelulaConway con5 = new CelulaConway(this,29,0);
+        CelulaConway con6 = new CelulaConway(this,29,1);
+        CelulaConway con7 = new CelulaConway(this,28,1);
+        CelulaConway con8 = new CelulaConway(this,28,0);
+    }
+    
+    private void prueba3(){
+        CelulaConway con9 = new CelulaConway(this,29,15);
+        CelulaConway con10 = new CelulaConway(this,29,16);
+        CelulaConway con11 = new CelulaConway(this,29,17);
+    }
+    
+    public void ticTac(){
+        for(Elemento[] array:automata){
+            for(Elemento elemento:array){
+                if(elemento != null){
+                    elemento.decida();
+                    elemento.cambie();
                 }
             }
         }
     }
+
 }
