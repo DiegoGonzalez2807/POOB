0044a9d5903b8ca6ed1c0bdf757a0b711b04f694
Diego Alejandro González <59978048+DiegoGonzalez2807@users.noreply.github.com>
2021-05-10 12:21:11 -0500

    Update Ruta.java

--- TALLER/Taller3Avance/Ruta.java
+++ TALLER/Taller3Avance/Ruta.java
@@ -20,17 +20,35 @@ public class Ruta {
 
     /**
      * Función que revisa si las dos estaciones están en la ruta
+     * Recordar que se debe mirar que no tenga transbordos, eso quiere decir que ambas
+     * estaciones estén en la misma ruta y no se tenga que coger otro bus
      * @param estacion_1
      * @param estacion_2
-     * @return
+     * @return --> ambas estaciones están en la ruta
      */
-    private boolean contiene_estaciones(String estacion_1, String estacion_2){
-        boolean safe = false;
-        /**for(estacion estacion:paradas){
-            if(estacion_1.equals(estacion.getName()) || estacion_2.equals(estacion.getName())){
-                safe = safe && true;
+    private boolean sinTransbordo(String estacion_1, String estacion_2){
+        boolean answer = false;
+        //CASO DONDE SOLO SE TIENE QUE ENCONTRAR LA ESTACION 1 Y 2
+        if(checkerEstacion(estacion_1) && checkerEstacion(estacion_2)){
+            answer = true;
+        }
+        return answer;
+    }
+
+    /**
+     * Función encargada de revisar si la estación que se está revisando está en
+     * las paradas de esta ruta
+     * @param estacionRevision
+     * @return --> si está en la ruta
+     */
+    private boolean checkerEstacion(String estacionRevision){
+        boolean answer = false;
+        for(Estacion estacion: paradas){
+            if(estacion.getName().equals(estacionRevision)){
+                answer = true;
+                break;
             }
-        }*/
-        return safe;
+        }
+        return answer;
     }
 }
