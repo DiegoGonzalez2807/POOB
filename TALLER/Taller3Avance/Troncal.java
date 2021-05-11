17125142a9615f38ff24beeb2de4b28b2854ba79
Diego Alejandro González <59978048+DiegoGonzalez2807@users.noreply.github.com>
2021-05-10 23:37:18 -0500

    Update Troncal.java

--- TALLER/Taller3Avance/Troncal.java
+++ TALLER/Taller3Avance/Troncal.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
+/**
+ * Clase que contiene la información de las troncales de Transmilenio
+ * @author Cristian Castellanos- Diego Gonzalez
+ */
+
+ /**
+  * Constructor de la clase Troncal
+  */
 public class Troncal {
-    public String getName(){
-        return "";
-    }
+    private String nombre;
+    private ArrayList<Estacion> estaciones;
+    private TreeMap<String[], Integer> tramos;
 
 }
